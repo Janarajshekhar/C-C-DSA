192347a9c40852ed74064edba628d71c685c5423
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-01-01 10:57:02 +0530

    two function add

--- practice.c
+++ practice.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+#include<math.h>
 void calculat_the_sum_of_first_n_natural_number_using_loop();
 void count_the_digites_of_a_integer();
 void find_the_revers_of_a_integer();
+void check_a_number_is_palindrom_or_not();
+void check_a_number_is_armstrong_number_or_not();
 int main()
 {
-    // calculat_the_sum_of_first_n_natural_number_using_loop();
-    // count_the_digites_of_a_integer();
+    calculat_the_sum_of_first_n_natural_number_using_loop();
+    count_the_digites_of_a_integer();
     find_the_revers_of_a_integer();
+    check_a_number_is_palindrom_or_not();
+    check_a_number_is_armstrong_number_or_not();
     return 0;
 }
 void calculat_the_sum_of_first_n_natural_number_using_loop()
@@ -42,4 +47,42 @@ void find_the_revers_of_a_integer()
         r=10*r+s;
     }
     printf("\n revers number is %d",r);
+}
+void check_a_number_is_palindrom_or_not()
+{
+    int a,s,r=0,b;
+    printf("\n Enter a number : ");
+    scanf("%d",&a);
+    b=a;
+    for(;a!=0;a=a/10)
+    {
+        s=a%10;
+        r=10*r+s;
+    }
+    if(b==r)
+    printf("\n %d is palindrom number",b);
+    else
+    printf("\n %d is not palindrom number",b);
+}
+void check_a_number_is_armstrong_number_or_not()
+{
+    int a,y,n=0,i,sum=0,d,p;
+    printf("\n");
+    printf("\n Enter a number : ");
+    scanf("%d",&a);
+    for(y=a;y;n++)
+    {
+        y=y/10;
+    }
+    for(y=a;y;y=y/10)
+    {
+        d=y%10;
+        for(p=1,i=1;i<=n;i++)
+            p=p*d;
+        sum=sum+p;
+    }
+    if(sum==a)
+        printf("%d is an armstrong number",a);
+    else
+        printf("%d is not an armstrong number",a);
 }
